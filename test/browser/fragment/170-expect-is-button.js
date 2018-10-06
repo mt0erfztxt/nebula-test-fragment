41c3fa977d9df6148a5f92093d3712b2066f7ed1
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-10-07 00:47:12 +0500

    added tests for Fragment#expectIsButton

---
+++ test/browser/fragment/170-expect-is-button.js
@@ -0,0 +1,76 @@
+import appRootPath from 'app-root-path';
+import sinon from 'sinon';
+import unexpected from 'unexpected';
+import unexpectedSinon from 'unexpected-sinon';
+
+import Fragment from '../../../src/fragment';
+
+const expect = unexpected.clone();
+expect.use(unexpectedSinon);
+
+fixture `Fragment :: 170 #expectIsButton()`
+  .page(appRootPath.path + '/test/fixtures/fragment/170-expect-is-button.html');
+
+class Btn extends Fragment {}
+
+Object.defineProperties(Btn, {
+  bemBase: {
+    value: 'btn'
+  },
+  displayName: {
+    value: 'Btn'
+  }
+});
+
+test("010 It should delegate to `#expectExistsAndConformsRequirements`", async () => {
+  const btn = new Btn();
+  const spy = sinon.spy(btn, 'expectExistsAndConformsRequirements');
+  await btn.expectIsButton();
+  expect(spy, 'was called times', 1);
+  expect(spy, 'to have a call satisfying', {
+    args: [
+      { tagName: 'button' },
+      { selector: void(0) }
+    ]
+  });
+});
+
+test("020 It should handle 'text' option", async () => {
+  const btn = new Btn();
+  const spy = sinon.spy(btn, 'expectExistsAndConformsRequirements');
+
+  // -- Case of string
+
+  await btn.expectIsButton({ text: 'Button' });
+  expect(spy, 'was called times', 1);
+  expect(spy, 'to have a call satisfying', {
+    args: [
+      { tagName: 'button', text: ['Button'] },
+      { selector: void(0) }
+    ]
+  });
+
+  // -- Case of array
+
+  await btn.expectIsButton({ text: ['Foo', true] });
+  expect(spy, 'was called times', 2);
+  expect(spy, 'to have a call satisfying', {
+    args: [
+      { tagName: 'button', text: ['Foo', true] },
+      { selector: void(0) }
+    ]
+  });
+});
+
+test("030 It should handle 'selector' option", async () => {
+  const btn = new Btn();
+  const spy = sinon.spy(btn, 'expectExistsAndConformsRequirements');
+  await btn.expectIsButton({ text: 'Bar Button', selector: '.bar-btn' });
+  expect(spy, 'was called times', 1);
+  expect(spy, 'to have a call satisfying', {
+    args: [
+      { tagName: 'button' },
+      { selector: '.bar-btn' }
+    ]
+  });
+});
