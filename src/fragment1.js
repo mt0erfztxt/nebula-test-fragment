d64dbbd95a60ddf10c69d0634344acdefcdef57f
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2018-11-21 01:10:23 +0500

    added tests for state based fragment's built-in transformations

--- src/fragment1.js
+++ src/fragment1.js
@@ -24,14 +24,21 @@ class Fragment {
       }
     });
 
-    // TODO Check `selectorInitializer` is supported.
-    // We initialize fragment's selector using its BEM base, which must be
-    // provided as fragment's class property.
-
+    // TODO Check `selectorInitializer` is supported - function, map or list of
+    //      functions and/or maps.
     /**
      * Value used to initialize fragment's selector.
+     * 
+     * @type {Array<*>}
      */
-    this.selectorInitializer = selectorInitializer || [];
+    this.selectorInitializer = [];
+
+    if (_.isArray(selectorInitializer)) {
+      this.selectorInitializer = selectorInitializer;
+    }
+    else if (!_.isNil(selectorInitializer)) {
+      this.selectorInitializer = [selectorInitializer];
+    }
 
     // Fragment must have BEM base. It can be provided as property of
     // fragment's class or ancestor fragment's class, and can be an instance of
@@ -194,7 +201,7 @@ class Fragment {
     const requestedTransformations = _
       .chain(transformations)
       .keys()
-      .filter(object.hasOwnProperty)
+      .filter(_.has)
       .value();
 
     const unsupportedTransformations = _.filter(
@@ -209,68 +216,64 @@ class Fragment {
       );
     }
 
-    for (const t in requestedTransformations) {
+    // Handle 'cns' (component namespace) transformation.
+    if (_.includes(requestedTransformations, 'cns')) {
+      const v = transformations.cns;
 
-      // 'cns' (component namespace) often used to build one widget on top
-      // of other widget.
-      if (t === 'cns') {
-        const v = transformations.cns;
-
-        if (!utils.isNonEmptyString(v)) {
-          throw new TypeError(
-            `${this.displayName}#transformSelector(): Built-in 'cns' ` +
-            `transformation must be a non-empty string but it is ` +
-            `${typeOf(v)} (${v})`
-          );
-        }
-
-        selector = selector
-          .filter(`.${bemBase.setMod(['cns', v], { fresh: true })}`);
+      if (!utils.isNonEmptyString(v)) {
+        throw new TypeError(
+          `${this.displayName}#transformSelector(): Built-in 'cns' ` +
+          `transformation must be a non-empty string but it is ` +
+          `${typeOf(v)} (${v})`
+        );
       }
 
-      // 'cid' (component id) often used to get specific fragment. It respects
-      // 'cns' transformation.
-      else if (t === 'cid') {
-        const v = transformations.cid;
+      selector = selector
+        .filter(`.${bemBase.setMod(['cns', v], { fresh: true })}`);
+    }
 
-        if (!utils.isNonEmptyString(v)) {
-          throw new TypeError(
-            `${this.displayName}#transformSelector(): Built-in 'cid'  ` +
-            `transformation must be a non-empty string but it is ` +
-            `${typeOf(v)} (${v})`
-          );
-        }
+    // Handle 'cid' (component id) transformation.
+    if (_.includes(requestedTransformations, 'cid')) {
+      const v = transformations.cid;
 
-        selector = selector
-          .filter(`.${bemBase.setMod(['cid', v], { fresh: true })}`);
+      if (!utils.isNonEmptyString(v)) {
+        throw new TypeError(
+          `${this.displayName}#transformSelector(): Built-in 'cid'  ` +
+          `transformation must be a non-empty string but it is ` +
+          `${typeOf(v)} (${v})`
+        );
       }
 
-      // 'idx' often used to get specific fragment by its index. It respects
-      // 'cns' and 'cid' transformations.
-      else if (t === 'idx') {
-        const v = transformations.idx;
+      selector = selector
+        .filter(`.${bemBase.setMod(['cid', v], { fresh: true })}`);
+    }
 
-        if (_.isInteger(v) && v >= 0) {
-          /*
-           * NOTE Don't use `Selector.nth()` because it doesn't work properly,
-           * namely, when you try to call `nth()` later, for example, in
-           * following transformation, selector would be reseted to state
-           * before `nth()` call in 'idx' transformation. Perhaps this is a
-           * bug, but tested only in TestCafe '0.16.2'. This misbehavior was
-           * discovered first time in '060-specs-composition' tests.
-           *
-           * THIS WOULD NOT WORK PROPERLY!!!
-           * selector = selector.nth(v);
-           */
-          selector = selector.filter((node, idx) => idx === v, { v });
-        }
-        else {
-          throw new TypeError(
-            `${this.displayName}#transformSelector(): Built-in 'idx' ` +
-            `transformation must be an integer greater than or equal zero ` +
-            `but it is ${typeOf(v)} (${v})`
-          );
-        }
+    // Handle 'idx' transformation. It respects 'cns' and 'cid' transformations
+    //  and so applied after them.
+    if (_.includes(requestedTransformations, 'idx')) {
+      const v = transformations.idx;
+
+      if (_.isInteger(v) && v >= 0) {
+        /*
+         * NOTE Don't use `Selector.nth()` because it doesn't work properly,
+         * namely, when you try to call `nth()` later, for example, in
+         * following transformation, selector would be reseted to state
+         * before `nth()` call in 'idx' transformation. Perhaps this is a
+         * bug, but tested only in TestCafe '0.16.2'. This misbehavior was
+         * discovered first time in '060-specs-composition' tests.
+         *
+         * THIS WOULD NOT WORK PROPERLY!!!
+         * selector = selector.nth(v);
+         */
+        selector = selector
+          .filter((_, idx) => idx === v, { v });
+      }
+      else {
+        throw new TypeError(
+          `${this.displayName}#transformSelector(): Built-in 'idx' ` +
+          `transformation must be an integer greater than or equal zero ` +
+          `but it is ${typeOf(v)} (${v})`
+        );
       }
     }
 
--- test/browser/fragment1/index.js
+++ test/browser/fragment1/index.js
@@ -5,6 +5,7 @@ import unexpectedSinon from 'unexpected-sinon';
 
 import bem from '../../../src/bem';
 import Fragment from '../../../src/fragment1';
+import selector from '../../../src/selector';
 
 const { BemBase } = bem;
 
@@ -32,7 +33,7 @@ fixture("Fragment");
 
 test
   .page(buildPagePath())
-  ("010 It chooses correct BEM base for fragment", async () => {
+  ("010 It should choose correct BEM base for fragment", async () => {
 
     // -- Check when BEM base on fragment itself
 
@@ -74,7 +75,7 @@ test
 
 test
   .page(buildPagePath('020'))
-  ("020 It creates fragment with selector returning only elements with fragment's BEM base", async (t) => {
+  ("020 It should create fragment with selector returning only elements with fragment's BEM base", async (t) => {
     const f1 = new F1();
     expect(f1._selector, 'to be defined');
     await t.expect(f1._selector.count).eql(2);
@@ -84,7 +85,7 @@ test
 
 test
   .page(buildPagePath('030'))
-  ("030 It scopes fragment's selector into parent selector", async (t) => {
+  ("030 It should scope fragment's selector into parent selector", async (t) => {
 
     // -- Check when parent not provided
 
@@ -124,7 +125,7 @@ test
 
 test
   .page(buildPagePath())
-  ("040 It initializes fragment's selector on first access", async () => {
+  ("040 It should initialize fragment's selector on first access", async () => {
     let sel;
     const f1 = new F1();
     const initializeSelectorSpy = sinon.spy(f1, '_initializeSelector');
@@ -143,4 +144,200 @@ test
     expect(sel, 'to be defined');
     expect(initializeSelectorSpy, 'was called times', 1);
     expect(f1._selectorInitialized, 'to be true');
+  });
+
+test
+  .page(buildPagePath('050'))
+  ("050 It should allow 'selectorInitializer' argument to be a function", async (t) => {
+    const f1 = new F1((sel, _) => selector.filterByText(sel, 'f1b'));
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1']);
+    await t.expect(f1.selector.textContent).eql('f1b');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.textContent).eql('foo');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected 'f1b' to deeply equal 'foo'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('060'))
+  ("060 It should allow 'selectorInitializer' argument to be a list of functions", async (t) => {
+    const f1 = new F1([
+      (sel, _) => selector.filterByText(sel, 'f1b'),
+      (sel, _) => sel.nth(3),
+    ]);
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1']);
+    await t.expect(f1.selector.textContent).eql('f1b');
+    await t.expect(f1.selector.getAttribute('data-nth')).eql('3');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.getAttribute('data-nth')).eql('4');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected '3' to deeply equal '4'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('070'))
+  ("070 It should allow 'selectorInitializer' argument to be a POJO with built-in 'cid' transformation", async (t) => {
+    const f1 = new F1({ cid: 'baz' });
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1', 'f1--cid_baz']);
+    await t.expect(f1.selector.textContent).eql('baz');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.textContent).eql('foo');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected 'baz' to deeply equal 'foo'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('080'))
+  ("080 It should allow 'selectorInitializer' argument to be a POJO with built-in 'cns' transformation", async (t) => {
+    const f1 = new F1({ cns: 'baz' });
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1', 'f1--cns_baz']);
+    await t.expect(f1.selector.textContent).eql('baz');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.textContent).eql('foo');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected 'baz' to deeply equal 'foo'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('090'))
+  ("090 It should allow 'selectorInitializer' argument to be a POJO with built-in 'idx' transformation", async (t) => {
+    const f1 = new F1({ idx: 2 });
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1']);
+    await t.expect(f1.selector.textContent).eql('baz');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.textContent).eql('abc');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected 'baz' to deeply equal 'abc'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('100'))
+  ("100 It should apply built-in transformations in following order 'cns' -> 'cid' -> 'idx'", async (t) => {
+    const f1 = new F1({ idx: 1, cns: 'bar', cid: '11' });
+
+    await t.expect(f1.selector.count).eql(1);
+    await t.expect(f1.selector.classNames).eql(['f1', 'f1--cns_bar', 'f1--cid_11']);
+    await t.expect(f1.selector.textContent).eql('bar 11 1');
+
+    // -- Ensure it fails as expected
+
+    let isThrown = false;
+
+    try {
+      await t.expect(f1.selector.textContent).eql('bar 11 0');
+    }
+    catch (e) {
+      expect(
+        e.errMsg,
+        'to match',
+        /.+expected 'bar 11 1' to deeply equal 'bar 11 0'/
+      );
+
+      isThrown = true;
+    }
+
+    expect(isThrown, 'to be true');
+  });
+
+test
+  .page(buildPagePath('110'))
+  ("110 It should allow 'selectorInitializer' argument to be a list of POJOs", async (t) => {
+    throw new Error('To be done!')
+  });
+
+test
+  .page(buildPagePath('120'))
+  ("120 It should allow 'selectorInitializer' argument to be a mix of function and POJOs", async (t) => {
+    throw new Error('To be done!')
+  });
+
+test
+  .page(buildPagePath('130'))
+  ("130 It should allow 'selectorInitializer' argument to have custom tranformations provided by derived fragments", async (t) => {
+    throw new Error('To be done!')
   });
