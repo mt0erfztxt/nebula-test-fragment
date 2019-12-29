52ff006d64601d08cd77e93416879c6088cceadc
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2019-12-29 23:51:42 +0500

    Add expectExistsAndConformsRequirements (WIP)

--- src/main/page-object.ts
+++ src/main/page-object.ts
@@ -4,9 +4,18 @@ import {
   BemBase,
   BemModifier,
   BemModifierName,
+  BemModifierRequirement,
   validateBemModifierName
 } from "./bem";
-import { SpecializedExpectDomElementsCountIs } from "./selector";
+import {
+  AttributeName,
+  AttributeRequirement,
+  filterByAttribute,
+  NegationFlag,
+  SpecializedExpectDomElementsCountIs
+} from "./selector";
+
+const escapeStringRegexp = require("escape-string-regexp");
 
 /**
  * Represents selector transformation.
@@ -717,5 +726,113 @@ export class PageObject {
     }
 
     return pageObjects;
+  }
+
+  /**
+   * Asserts that page object's selector returns exactly one DOM element and
+   * that element conforms specified requirements.
+   *
+   * @param [requirements] Requirements
+   * @param [requirements.attributes] Allows to assert that page object's selector returned DOM element have or have no attributes specified by {@link AttributeRequirement} -- uses same syntax as {@link filterByAttribute}
+   * @param [requirements.bemModifiers] Allows to assert that page object's selector returned DOM element have or have no BEM modifiers specified by {@link BemModifierRequirement}. Same as for `requirements.attributes` but instead of attribute's name and value BEM modifier's name and value used.
+   * @param [requirements.tagName] Allows to assert that page object's selector returned DOM element rendered using specified HTML tag
+   * @param [requirements.text] Allows to assert that page object's selector returned DOM element's text equal or matches specified value. Condition of assertion can be reversed by passing `Array` where first element is a text and second is a flag that specifies whether condition must be negated or not
+   * @param [requirements.textContent] Allows to assert that page object's selector returned DOM element's text content equal or matches specified value. Condition of assertion can be reversed by passing `Array` where first element is a text content and second is a flag that specifies whether condition must be negated or not
+   * @param [options] Options
+   * @param [options.selector=this.selector] TestCafe's `Selector` to assert on, page object's selector by default
+   */
+  async expectExistsAndConformsRequirements(
+    requirements?: {
+      attributes?: (AttributeRequirement | AttributeName)[];
+      bemModifiers?: (BemModifierRequirement | BemModifierName)[];
+      tagName?: string;
+      text?: string | RegExp | [string | RegExp, NegationFlag?];
+      textContent?: string | RegExp | [string | RegExp, NegationFlag?];
+    },
+    options?: { selector?: Selector }
+  ): Promise<void> {
+    options = options || {};
+    if (is.undefined(options.selector)) {
+      options.selector = this.selector;
+    }
+
+    let { selector } = options;
+    const elementsCount = await selector.count;
+    await t
+      .expect(elementsCount)
+      .eql(
+        1,
+        `Selector must return exactly one DOM element but it returned ` +
+          `${elementsCount} of them`
+      );
+
+    if (is.nullOrUndefined(requirements)) {
+      return;
+    }
+
+    // Attributes asserted by one at a time.
+    for (const item of requirements.attributes || []) {
+      const [attrName, attrValue, isNot = false] = is.array(item)
+        ? item
+        : [item];
+      selector = filterByAttribute(selector, [attrName, attrValue, isNot]);
+      await t
+        .expect(selector.count)
+        .eql(
+          1,
+          `Expected ${this.displayName}'s selector to ` +
+            (isNot ? "not" : "") +
+            `return DOM element with attribute '${attrName}'` +
+            (is.undefined(attrValue) ? "" : ` valued '${attrValue}'`)
+        );
+    }
+
+    // BEM modifiers asserted by one at a time.
+    for (const item of requirements.bemModifiers || []) {
+      const [modName, modValue, isNot = false] = is.array(item) ? item : [item];
+      if (isNot) {
+        await this.expectHasNoBemModifier([modName, modValue]);
+      } else {
+        await this.expectHasBemModifier([modName, modValue]);
+      }
+    }
+
+    if (requirements.tagName) {
+      await t.expect(selector.tagName).eql(requirements.tagName);
+    }
+
+    const { text } = requirements;
+    if (!is.undefined(text)) {
+      const [textOrRegExp, isNot = false] = is.array(text) ? text : [text];
+
+      // XXX TestCafe (v0.16.2) always converts `withText` argument to `RegExp`
+      // and so we must use workaround to use string equality.
+      const expectedValueAsRegExp = is.regExp(textOrRegExp)
+        ? textOrRegExp
+        : new RegExp(`^${escapeStringRegexp("" + textOrRegExp)}$`);
+      await t
+        .expect(selector.withText(expectedValueAsRegExp).count)
+        .eql(
+          isNot ? 0 : 1,
+          `${this.displayName}'s selector returned DOM element text must ` +
+            (isNot ? "not " : "") +
+            (is.regExp(textOrRegExp) ? "match" : "equal") +
+            ` ${expectedValueAsRegExp}`
+        );
+    }
+
+    const { textContent } = requirements;
+    if (!is.undefined(textContent)) {
+      const [stringOrRegExp, isNot = false] = is.array(textContent)
+        ? textContent
+        : [textContent];
+      if (is.regExp(stringOrRegExp)) {
+        const assertion = isNot ? "notMatch" : "match";
+        await t.expect(selector.textContent)[assertion](stringOrRegExp);
+      } else {
+        const assertion = isNot ? "notEql" : "eql";
+        await t.expect(selector.textContent)[assertion](stringOrRegExp);
+      }
+    }
   }
 }
