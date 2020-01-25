37113847c1de0d398a26545e3d30667f3ea59ea3
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2020-01-25 11:05:23 +0500

    Convert BEM module to TypeScript (WIP)

--- src/__tests__/bem.ts
+++ src/__tests__/bem.ts
@@ -1,82 +1,72 @@
-import { isBemModifier, isBemName, isBemValue } from "../bem";
+import { isValidBemModifier, isValidBemName, isValidBemValue } from "../bem";
 
-describe("isBemName()", () => {
-  test("returns false when value is not a string", () => {
-    expect(isBemName(42)).toBe(false);
+describe("isValidBemName()", () => {
+  test("returns false when value is a string that starts not with a letter", () => {
+    expect(isValidBemName("1")).toBe(false);
   });
 
-  test("returns false when value is not a string that starts with a letter", () => {
-    expect(isBemName("1")).toBe(false);
-  });
-
-  test("returns false when value is not a string that ends with a letter or a digit", () => {
-    expect(isBemName("a1-")).toBe(false);
+  test("returns false when value is a string that ends with not a letter or a digit", () => {
+    expect(isValidBemName("a1-")).toBe(false);
   });
 
   test("returns false when value is a string but not an alpha-numeric-dashed string", () => {
-    expect(isBemName("a__1")).toBe(false);
-    expect(isBemName("a@1")).toBe(false);
+    expect(isValidBemName("a__1")).toBe(false);
+    expect(isValidBemName("a@1")).toBe(false);
   });
 
   test("returns false when value is an alpha-numeric-dashed string but have sibling dashes", () => {
-    expect(isBemName("some--thing")).toBe(false);
+    expect(isValidBemName("some--thing")).toBe(false);
   });
 
   test("returns true when value meet all criteria of valid BEM name", () => {
-    expect(isBemName("name")).toBe(true);
-    expect(isBemName("name-with-dashes")).toBe(true);
-    expect(isBemName("name-that-ends-with-digit-1")).toBe(true);
+    expect(isValidBemName("name")).toBe(true);
+    expect(isValidBemName("name-with-dashes")).toBe(true);
+    expect(isValidBemName("name-that-ends-with-digit-1")).toBe(true);
   });
 });
 
-describe("isBemValue()", () => {
-  test("returns false when value is not a string", () => {
-    expect(isBemValue(42)).toBe(false);
-  });
-
-  test("returns false when value is not a string that ends with a letter or a digit", () => {
-    expect(isBemValue("a1-")).toBe(false);
+describe("isValidBemValue()", () => {
+  test("returns false when value is a string that ends with not a letter or a digit", () => {
+    expect(isValidBemValue("a1-")).toBe(false);
   });
 
   test("returns false when value is a string but not an alpha-numeric-dashed string", () => {
-    expect(isBemValue("a__1")).toBe(false);
-    expect(isBemValue("a@1")).toBe(false);
+    expect(isValidBemValue("a__1")).toBe(false);
+    expect(isValidBemValue("a@1")).toBe(false);
   });
 
   test("returns false when value is an alpha-numeric-dashed string but have sibling dashes", () => {
-    expect(isBemValue("some--thing")).toBe(false);
+    expect(isValidBemValue("some--thing")).toBe(false);
   });
 
   test("returns true when value meet all criteria of valid BEM name", () => {
-    expect(isBemValue("1")).toBe(true);
-    expect(isBemValue("value")).toBe(true);
-    expect(isBemValue("value-with-dashes")).toBe(true);
-    expect(isBemValue("value-that-ends-with-digit-1")).toBe(true);
-    expect(isBemValue("2-value-that-ends-with-digit")).toBe(true);
+    expect(isValidBemValue("1")).toBe(true);
+    expect(isValidBemValue("value")).toBe(true);
+    expect(isValidBemValue("value-with-dashes")).toBe(true);
+    expect(isValidBemValue("value-that-ends-with-digit-1")).toBe(true);
+    expect(isValidBemValue("2-value-that-ends-with-digit")).toBe(true);
   });
 });
 
-describe("isBemModifier()", () => {
-  test("returns false when value is not an array", () => {
-    expect(isBemModifier("fiz")).toBe(false);
+describe("isValidBemModifier()", () => {
+  test("returns false when value is an array but its first element is not valid BEM name", () => {
+    expect(isValidBemModifier(["1"])).toBe(false);
   });
 
-  test("returns false when value is an array but its first element is not a BEM name", () => {
-    expect(isBemModifier([1])).toBe(false);
-  });
-
-  test("returns false when value is an array but its second element is not a nil or a BEM value", () => {
-    expect(isBemModifier(["fiz", 1])).toBe(false);
-    expect(isBemModifier(["fiz", ""])).toBe(false);
-    expect(isBemModifier(["fiz", "-buz"])).toBe(false);
-    expect(isBemModifier(["fiz", "buz-"])).toBe(false);
+  test("returns false when value is an array but its second element is not a nil or valid BEM value", () => {
+    expect(isValidBemModifier(["fiz", ""])).toBe(false);
+    expect(isValidBemModifier(["fiz", " "])).toBe(false);
+    expect(isValidBemModifier(["fiz", "\t"])).toBe(false);
+    expect(isValidBemModifier(["fiz", "-buz"])).toBe(false);
+    expect(isValidBemModifier(["fiz", "buz-"])).toBe(false);
+    expect(isValidBemModifier(["fiz", "biz--buz"])).toBe(false);
   });
 
   test("returns true when value is a BEM modifier", () => {
-    expect(isBemModifier(["fiz"])).toBe(true);
-    expect(isBemModifier(["fiz", undefined])).toBe(true);
-    expect(isBemModifier(["fiz", null])).toBe(true);
-    expect(isBemModifier(["fiz", "1"])).toBe(true);
-    expect(isBemModifier(["fiz", "buz"])).toBe(true);
+    expect(isValidBemModifier(["fiz"])).toBe(true);
+    expect(isValidBemModifier(["fiz", undefined])).toBe(true);
+    expect(isValidBemModifier(["fiz", "1"])).toBe(true);
+    expect(isValidBemModifier(["fiz", "buz"])).toBe(true);
+    expect(isValidBemModifier(["fiz", "biz-buz"])).toBe(true);
   });
 });
--- src/bem.ts
+++ src/bem.ts
@@ -1,87 +1,91 @@
 import is from "@sindresorhus/is";
 
-/**
- * Returns `true` when `value` is a BEM name, otherwise returns `false`.
- *
- * BEM name -- a non-empty alpha-numeric-dashed string that starts and ends with
- * a letter, and doesn't contain sibling dashes.
- */
-export function isBemName(value: unknown): boolean {
-  if (is.string(value)) {
-    return (
-      /^[a-zA-Z](:?[a-zA-Z0-9-]*[a-zA-Z0-9])$/.test(value) &&
-      !/-{2,}/.test(value)
-    );
-  } else {
-    return false;
-  }
-}
+export type BemName = string;
+export type BemValue = string;
+
+export type BemBlock = BemName;
+export type BemElement = BemName;
+export type BemModifier = [BemName, BemValue?];
+
+export type BemObject = { blk: BemBlock; elt?: BemElement; mod?: BemModifier };
+export type BemString = string;
+export type BemVector = [BemBlock, BemElement?, BemModifier?];
 
 /**
- * Returns `true` when `value` is a BEM value, otherwise returns `false`.
- *
- * BEM value -- a non-empty alpha-numeric-dashed string that starts and ends
- * with a letter or a digit and doesn't contain sibling dashes.
+ * Returns `true` when `value` is valid BEM name -- a non-empty
+ * alpha-numeric-dashed string that starts and ends with a letter, and doesn't
+ * contain sibling dashes, otherwise returns `false`.
  */
-export function isBemValue(value: unknown): boolean {
-  if (is.string(value)) {
-    return (
-      /^(:?[a-zA-Z0-9]|[a-zA-Z0-9](:?[a-zA-Z0-9-]*[a-zA-Z0-9]))$/.test(value) &&
-      !/-{2,}/.test(value)
-    );
-  } else {
-    return false;
-  }
-}
-
-/**
- * Returns `true` when `value` is a BEM block, otherwise returns `false`.
- *
- * BEM block -- just a BEM name.
- */
-export function isBemBlock(value: unknown) {
-  return isBemName(value);
-}
-
-/**
- * Returns `true` when `value` is a BEM element, otherwise returns `false`.
- *
- * BEM element -- just a BEM name.
- */
-export function isBemElement(value: unknown) {
-  return isBemName(value);
-}
-
-/**
- * Returns `true` when `value` is a BEM modifier, otherwise returns `false`.
- *
- * BEM modifier -- an array of required BEM modifier name and optional BEM
- * modifier value.
- */
-export function isBemModifier(value: unknown) {
+export function isValidBemName(value: BemName): boolean {
   return (
-    is.array(value) &&
-    isBemModifierName(value[0]) &&
-    (is.nullOrUndefined(value[1]) || isBemModifierValue(value[1]))
+    /^[a-zA-Z](:?[a-zA-Z0-9-]*[a-zA-Z0-9])$/.test(value) && !/-{2,}/.test(value)
   );
 }
 
 /**
- * Returns `true` when `value` is a BEM modifier name, otherwise returns
- * `false`.
- *
- * BEM modifier name -- just a BEM name.
+ * Returns `true` when `value` is valid BEM value -- a non-empty
+ * alpha-numeric-dashed string that starts and ends with a letter or a digit
+ * and doesn't contain sibling dashes, otherwise returns `false`.
  */
-export function isBemModifierName(value: unknown) {
-  return isBemName(value);
+export function isValidBemValue(value: BemValue): boolean {
+  return (
+    /^(:?[a-zA-Z0-9]|[a-zA-Z0-9](:?[a-zA-Z0-9-]*[a-zA-Z0-9]))$/.test(value) &&
+    !/-{2,}/.test(value)
+  );
 }
 
 /**
- * Returns `true` when `value` is a BEM modifier value, otherwise returns
- * `false`.
- *
- * BEM modifier value -- just a BEM value.
+ * Returns `true` when `value` is valid BEM block -- just a BEM name, otherwise
+ * returns `false`.
  */
-export function isBemModifierValue(value: unknown) {
-  return isBemValue(value);
+export function isValidBemBlock(value: BemBlock): boolean {
+  return isValidBemName(value);
+}
+
+/**
+ * Returns `true` when `value` is valid BEM element -- just a BEM name,
+ * otherwise returns `false`.
+ */
+export function isValidBemElement(value: BemElement): boolean {
+  return isValidBemName(value);
+}
+
+/**
+ * Returns `true` when `value` is valid BEM modifier -- an array of required
+ * BEM modifier name and optional BEM modifier value, otherwise returns `false`.
+ */
+export function isValidBemModifier(value: BemModifier): boolean {
+  return (
+    is.array(value) &&
+    isValidBemModifierName(value[0]) &&
+    (is.nullOrUndefined(value[1]) || isValidBemModifierValue(value[1]))
+  );
+}
+
+/**
+ * Returns `true` when `value` is valid BEM modifier name -- just a BEM name,
+ * otherwise returns `false`.
+ */
+export function isValidBemModifierName(value: BemName): boolean {
+  return isValidBemName(value);
+}
+
+/**
+ * Returns `true` when `value` is valid BEM modifier value -- just a BEM value,
+ * otherwise returns `false`.
+ */
+export function isValidBemModifierValue(value: BemValue): boolean {
+  return isValidBemValue(value);
+}
+
+/**
+ * Returns `true` when `value` is valid BEM object, otherwise returns `false`.
+ */
+export function isValidBemObject(value: BemObject): boolean {
+  const { blk, elt, mod } = value;
+  return (
+    isValidBemBlock(blk) &&
+    (is.nullOrUndefined(elt) || isValidBemElement(elt)) &&
+    (is.nullOrUndefined(mod) || isValidBemModifier(mod))
+  );
 }
