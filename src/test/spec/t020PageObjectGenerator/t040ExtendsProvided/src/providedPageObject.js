03e0a942ab06a4b96717f84532a0505870f276b6
Sergey Kozhevnikov <mt0erfztxt@gmail.com>
2020-02-29 18:58:36 +0500

    Track accidentally ignored file

---
+++ src/test/spec/t020PageObjectGenerator/t040ExtendsProvided/src/providedPageObject.js
@@ -0,0 +1,6 @@
+import PageObject from "nebula-test-fragment/lib/pageObject";
+
+export default class ProvidedPageObject extends PageObject {
+  static bemBase = "";
+  static displayName = "ProvidedPageObject";
+}
